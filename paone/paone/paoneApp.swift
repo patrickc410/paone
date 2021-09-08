930346616b81d98fe362e102f85a7e78e30c5c1a
Patrick Crawford <pac410@gmail.com>
2021-09-08 18:51:36 -0400

    its not working

---
+++ paone/paone/paoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  paoneApp.swift
+//  paone
+//
+//  Created by Patrick Crawford on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct paoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
